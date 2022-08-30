16f0d01897d008ca2c09d09892733fd68bcf4ce4
luisgontero28 <luisgontero2000@gmail.com>
2022-08-29 23:09:37 -0300

    mensaje

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+
+function saludar(nombre, apellido){
+    alert("hola " + nombre + " " + apellido + " como estas");
+}
+let ingresaNombre = prompt("Ingresa nombre")
+let ingresaApellido = prompt("Ingresa apellido")
+saludar(ingresaNombre, ingresaApellido)
+
 // filtro por edad
 let edad = parseFloat(prompt("ingresa tu edad"))
 if(edad >= 18){ 
@@ -32,3 +40,87 @@ while(socio == si){
     console.log("podes ingresar, sos socio");
     socio = parseFloat(prompt("sos socio:si o no"))
 }
+
+
+
+//switch
+let ingreso = prompt("Ingresa una opcion \n 1=Carbon \n 2=Leña \n 3=Postes \n 4=Tablas \n 5=Varillas"
+);
+switch(ingreso){
+    case "1":
+        alert("Seleccionaste carbon");
+        break;
+    case "2":
+        alert("Seleccionaste Leña");
+        break;
+    case "3":
+        alert("Seleccionaste postes");
+        break;
+    default:
+        alert("Sin stock");
+        break;
+}
+
+//con numeros, hacen operacion de sumar
+function sumar(parametro1, parametro2){
+    if (isNaN(parametro1) || isNaN(parametro2)){
+        alert("Los valores ingresados deben se numeros");
+    } else{
+        alert("Son " + parametro1 + parametro2 + "productos.");
+    }
+}
+let num1 = parseFloat(prompt("ingrese la cantidad de postes."));
+let num2 = parseFloat(prompt("ingrese la cantidad de varillas."))
+sumar(num1, num2);
+
+
+
+//funciones
+/*
+//otro ej de funcion
+function solicitarNombre(){
+    let nombre = prompt("Ingresa tu nombre")
+    alert(nombre)
+}
+solicitarNombre()
+*/
+//Funciones con parametros
+/*//
+function saludar(nombre){
+    alert("hola " + nombre + " como estas");
+}
+saludar("Martin");
+saludar("luis");
+*/
+/*
+// esto tambien se puede usar con numero y que haga calculos
+function saludar(nombre, apellido){
+    alert("hola " + nombre + " " + apellido + " como estas");
+}
+let ingresaNombre = prompt("Ingresa nombre")
+let ingresaApellido = prompt("Ingresa apellido")
+saludar(ingresaNombre, ingresaApellido)
+*/
+/*
+//con numeros, hacen operacion de sumar
+function sumar(parametro1, parametro2){
+    if (isNaN(parametro1) || isNaN(parametro2)){
+        alert("Los valores ingresados deben se numeros");
+    } else{
+        alert(parametro1 + parametro2);
+    }
+}
+let num1 = parseFloat(prompt("ingrese el primer numero"));
+let num2 = parseFloat(prompt("ingrese el segundo numero"))
+sumar(num1, num2);
+*/
+//funciones
+/*
+//declarar funciones
+function saludar(){
+    //codigo a ejecutar
+    alert("Hola como estas");
+}
+//llamar a la funcion, lo puedo usar en la linea que quiera para no escribir tanto codigo
+saludar();
+*/
